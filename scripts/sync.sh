849adbb35aac8d812e15564623ce4ecc6ee82e96
bswck <bswck.dev@gmail.com>
2023-11-24 11:28:11 +0100

    Upgrade to bswck/skeleton@5d04f93
    Skeleton revision: https://github.com/bswck/skeleton/tree/5d04f93

--- scripts/sync.sh
+++ scripts/sync.sh
@@ -2,7 +2,7 @@
 # (C) 2023–present Bartosz Sławecki (bswck)
 #
 # Sync with bswck/skeleton.
-# This script was adopted from https://github.com/bswck/skeleton/tree/678468f/project/scripts/sync.sh.jinja
+# This script was adopted from https://github.com/bswck/skeleton/tree/5d04f93/project/scripts/sync.sh.jinja
 #
 # Usage:
 # $ poe sync
@@ -10,7 +10,7 @@
 # shellcheck disable=SC2005
 
 
-# Automatically copied from https://github.com/bswck/skeleton/tree/678468f/handle-task-event.sh
+# Automatically copied from https://github.com/bswck/skeleton/tree/5d04f93/handle-task-event.sh
 
 toggle_workflows() {
     # Toggle workflows depending on the project's settings
@@ -117,7 +117,7 @@ after_update_algorithm() {
 }
 
 main() {
-    export LAST_REF="678468f"
+    export LAST_REF="5d04f93"
     export PROJECT_PATH_KEY="$$_skeleton_project_path"
     export NEW_REF_KEY="$$_skeleton_new_ref"
     export LAST_LICENSE_NAME="MIT"
