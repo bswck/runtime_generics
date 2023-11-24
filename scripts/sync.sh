9e07a7bc5e0ceb1b533e6888f55dccdda1c0f719
bswck <bswck.dev@gmail.com>
2023-11-24 11:35:15 +0100

    Upgrade to bswck/skeleton@33acf49
    Skeleton revision: https://github.com/bswck/skeleton/tree/33acf49

--- scripts/sync.sh
+++ scripts/sync.sh
@@ -2,7 +2,7 @@
 # (C) 2023–present Bartosz Sławecki (bswck)
 #
 # Sync with bswck/skeleton.
-# This script was adopted from https://github.com/bswck/skeleton/tree/5d04f93/project/scripts/sync.sh.jinja
+# This script was adopted from https://github.com/bswck/skeleton/tree/33acf49/project/scripts/sync.sh.jinja
 #
 # Usage:
 # $ poe sync
@@ -10,7 +10,7 @@
 # shellcheck disable=SC2005
 
 
-# Automatically copied from https://github.com/bswck/skeleton/tree/5d04f93/handle-task-event.sh
+# Automatically copied from https://github.com/bswck/skeleton/tree/33acf49/handle-task-event.sh
 
 toggle_workflows() {
     # Toggle workflows depending on the project's settings
@@ -97,15 +97,10 @@ after_update_algorithm() {
             local COMMIT_MSG="Upgrade to bswck/skeleton of unknown revision"
         fi
     fi
-    while test "$(echo "$(git diff --check)")"
-    do
-        echo "Cannot commit with the following conflicts:"
-        echo "$(git diff --check)"
-        echo "Please resolve the conflicts and press Enter to continue."
-        read -r
-    done
     redis-cli del "$PROJECT_PATH_KEY" > /dev/null 2>&1
     redis-cli del "$NEW_REF_KEY" > /dev/null 2>&1
+    echo "Press ENTER to commit the changes or CTRL+C to abort."
+    read -r || exit 1
     git commit --no-verify -m "$COMMIT_MSG" -m "$REVISION_PARAGRAPH"
     git push --no-verify
     toggle_workflows
@@ -117,7 +112,7 @@ after_update_algorithm() {
 }
 
 main() {
-    export LAST_REF="5d04f93"
+    export LAST_REF="33acf49"
     export PROJECT_PATH_KEY="$$_skeleton_project_path"
     export NEW_REF_KEY="$$_skeleton_new_ref"
     export LAST_LICENSE_NAME="MIT"
